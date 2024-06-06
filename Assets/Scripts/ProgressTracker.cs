69d5863b9b14db8b3086b3512c254ef764ed9d9e
Anıl <47008814+anlmdk@users.noreply.github.com>
2024-06-07 02:38:28 +0300

    added slider mechanic

---
+++ Assets/Scripts/ProgressTracker.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressTracker : MonoBehaviour
+{
+    public Transform character; // Karakterin Transform bileþeni
+    public Transform startPoint; // Yolun baþlangýç noktasý
+    public Transform endPoint; // Yolun bitiþ noktasý
+    public Slider progressSlider; // Ýlerleme durumu için slider
+
+    void Update()
+    {
+        // Karakterin baþlangýç noktasýndan bitiþ noktasýna olan ilerlemesini hesapla
+        float totalDistance = Vector3.Distance(startPoint.position, endPoint.position);
+        float currentDistance = Vector3.Distance(startPoint.position, character.position);
+
+        // Ýlerlemeyi 0 ile 1 arasýnda normalize et
+        float progress = Mathf.Clamp01(currentDistance / totalDistance);
+
+        // Slider'ý güncelle
+        progressSlider.value = progress;
+    }
+}
